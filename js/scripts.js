440af7edfd719565c55d70f2a99bb704770b8226
Slavcho Mitrov <slavemitrov@yahoo.com>
2017-06-02 22:28:02 +0200

    The ace way

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,6 @@
 $(document).ready(function() {
   $("ul").on("click","li",function() {
-    if ($.trim($(this).html()) === 'Turtles') {
-      $(".thumbnail").hide();
-      $("#turtles").show();
-    } else if ($.trim($(this).html()) === 'Snakes') {
-      $(".thumbnail").hide();
-      $("#snakes").show();
-    } else {
-      $(".thumbnail").hide();
-      $("#tigers").show();
-    }
+  	$(".thumbnail").hide();
+    $("#" + $(this).data("details-id")).show();
   });
 });
